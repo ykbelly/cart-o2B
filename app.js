4445e03ecc7045067f4e74ae15e34200a3466c67
ykbelly <142520325+ykbelly@users.noreply.github.com>
2024-10-30 11:03:19 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+function criaCartao(categoria,pergunta,resposta){
+let container = document.getElementById('continer')
+
+}
+
---
+++ perguntas.js
@@ -0,0 +1,36 @@
+//variaveis
+
+//funções
+
+//chamada
+
+criaCartao(
+    'Programação' ,
+    'O que é JavaScript?' ,
+    'O javaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Literatura' ,
+    'Qual o nome da Saga d Arlan Fardos' ,
+    'O nome é Ciclo das Trevas'
+)
+
