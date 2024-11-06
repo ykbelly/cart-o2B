abbae417bdaae70422b47a2751d134d5578675fe
ykbelly <142520325+ykbelly@users.noreply.github.com>
2024-11-06 10:35:58 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-function criaCartao(categoria,pergunta,resposta){
+function criaCartao(categoria,pergunta,resposta) {
 let container = document.getElementById('continer')
+let cartao = document.createElement('article')
+cartao.className = 'cartao'
+
+cartao.innerHTML = `
+<div class="cartao__conteudo">
+</div>`
 
 }
 
