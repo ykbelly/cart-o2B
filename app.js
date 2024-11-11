f24e52c36f0010c8177a4bce04a826ae6c67ce38
ykbelly <142520325+ykbelly@users.noreply.github.com>
2024-11-11 09:09:02 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function criaCartao(categoria,pergunta,resposta) {
-let container = document.getElementById('continer')
+let container = document.getElementById('container')
 let cartao = document.createElement('article')
 cartao.className = 'cartao'
 
